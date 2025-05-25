d1443816fabcd4f863b46095f67c59d9c7ed8bc2
dev <dev@A24PC2>
2025-05-25 15:35:45 +0300

    add swagger

---
+++ WebApiCarsBids/WebApiCarsBids/Program.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
