253309a0097606a711c2248ed9f667c5b4543858
Reap4ick <122297692+Reap4ick@users.noreply.github.com>
2025-08-22 23:50:54 +0300

    Resolve

--- CarsAndBids.Core/Entities/User.cs
+++ CarsAndBids.Core/Entities/User.cs
@@ -4,9 +4,9 @@ namespace CarsAndBids.Core.Entities;
 
 public class User : IdentityUser<int>
 {
-    public string? Bio { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public string? ProfilePictureUrl { get; set; }
+    public string? Bio { get; set; }
 
     public ICollection<Car>? AssingCars { get; set; }
     public ICollection<Car>? OwnedCars { get; set; }
@@ -19,5 +19,5 @@ public class User : IdentityUser<int>
     public ICollection<Wishlist>? Wishlists { get; set; }
     public ICollection<RefreshToken>? RefreshTokens { get; set; }
     public ICollection<Chat>? Chats { get; set; }
-    public ICollection<UserChatMessageReaction>? UserChatMessageReactions { get; set; }    
+    public ICollection<UserChatMessageReaction>? UserChatMessageReactions { get; set; }
 }
