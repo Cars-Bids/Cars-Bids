caa6789b02b477c02d9af30287e1b5a50cf2df52
Reap4ick <122297692+Reap4ick@users.noreply.github.com>
2025-10-01 02:57:29 +0300

    Fix

--- Steria.Core/CQRS/Auctions/GetFilteredAuctionsQuery.cs
+++ Steria.Core/CQRS/Auctions/GetFilteredAuctionsQuery.cs
@@ -29,50 +29,15 @@ public class GetFilteredAuctionsQueryHandler(
 {
     public async Task<FilteredAuctionsPagedResult> Handle(GetFilteredAuctionsQuery request, CancellationToken cancellationToken)
     {
-        // Створюємо специфікацію для фільтрації та пагінації
         var spec = new FilteredAuctionsSpec(request);
         var auctions = await auctionRepository.GetListBySpec(spec, cancellationToken);
 
-        // Мапимо аукціони на DTO
         var auctionDtos = mapper.Map<List<AuctionFilteredDto>>(auctions);
 
-        // Отримуємо унікальні ModelId і MakeId
         var modelIds = auctions.Select(a => a.Car.ModelId).Distinct().ToList();
         var makeIds = auctions.Select(a => a.Car.Model.MakeId).Distinct().ToList();
 
-        // Отримуємо загальну кількість для пагінації
-        var countSpec = new Specification<Auction>();
-        countSpec.Query.Where(a => a.Status == AuctionStatus.Active);
-
-        if (!string.IsNullOrWhiteSpace(request.Transmission))
-        {
-            if (Enum.TryParse<TransmissionType>(request.Transmission, true, out var transmissionType))
-            {
-                countSpec.Query.Where(a => a.Car.TransmissionType == transmissionType);
-            }
-        }
-        if (!string.IsNullOrWhiteSpace(request.BodyStyle))
-        {
-            countSpec.Query.Where(a => a.Car.BodyStyle.StyleName == request.BodyStyle);
-        }
-        if (request.MinMileage.HasValue)
-        {
-            countSpec.Query.Where(a => a.Car.Mileage >= request.MinMileage.Value);
-        }
-        if (request.MaxMileage.HasValue)
-        {
-            countSpec.Query.Where(a => a.Car.Mileage <= request.MaxMileage.Value);
-        }
-        if (!string.IsNullOrWhiteSpace(request.MakeModelSearch))
-        {
-            var searchTerms = request.MakeModelSearch.Trim().ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            foreach (var term in searchTerms)
-            {
-                countSpec.Query.Where(a => a.Car.Model.Name.ToLower().Contains(term) ||
-                                        a.Car.Model.Make.Name.ToLower().Contains(term));
-            }
-        }
-
+        var countSpec = new FilteredAuctionsCountSpec(request);
         var totalCount = await auctionRepository.CountAsync(countSpec, cancellationToken);
 
         return new FilteredAuctionsPagedResult
---
+++ Steria.Core/Specification/AuctionSpec/FilteredAuctionsCountSpec.cs
@@ -0,0 +1,48 @@
+﻿using Ardalis.Specification;
+using Steria.Core.CQRS.Auctions;
+using Steria.Core.Entities;
+using Steria.Core.Enums;
+
+namespace Steria.Core.Specification.AuctionSpec;
+
+public class FilteredAuctionsCountSpec : Specification<Auction>
+{
+    public FilteredAuctionsCountSpec(GetFilteredAuctionsQuery query)
+    {
+        //Query.Where(a => a.Status == AuctionStatus.Active);
+
+        if (!string.IsNullOrWhiteSpace(query.Transmission))
+        {
+            if (Enum.TryParse<TransmissionType>(query.Transmission, true, out var transmissionType))
+            {
+                Query.Where(a => a.Car.TransmissionType == transmissionType);
+            }
+        }
+
+        if (!string.IsNullOrWhiteSpace(query.BodyStyle))
+        {
+            Query.Where(a => a.Car.BodyStyle.StyleName == query.BodyStyle);
+        }
+
+        if (query.MinMileage.HasValue)
+        {
+            Query.Where(a => a.Car.Mileage >= query.MinMileage.Value);
+        }
+        if (query.MaxMileage.HasValue)
+        {
+            Query.Where(a => a.Car.Mileage <= query.MaxMileage.Value);
+        }
+
+        if (!string.IsNullOrWhiteSpace(query.MakeModelSearch))
+        {
+            var searchTerms = query.MakeModelSearch.Trim().ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            foreach (var term in searchTerms)
+            {
+                Query.Where(a => a.Car.Model.Name.ToLower().Contains(term) ||
+                                a.Car.Model.Make.Name.ToLower().Contains(term));
+            }
+        }
+
+        Query.AsNoTracking();
+    }
+}
